9f7962d332a525cbd62a54bd4828ced23cc0e971
Alberto Soutullo <alberto.soutullo639@gmail.com>
2023-10-18 12:26:47 +0200

    Modified runner

--- runner.sh
+++ runner.sh
@@ -8,10 +8,12 @@ if ! docker network inspect "$custom_network_name" >/dev/null 2>&1; then
   docker network create --attachable --driver bridge "$custom_network_name"
 fi
 
-for ((i = 1; i <= num_peers; i++)); do
+for ((i = 0; i < num_peers; i++)); do
     # Construct the hostname (e.g., peer1, peer2, ...)
-    hostname="peer$i"
+    hostname="peer-$i"
 
     # Run the Docker container with the current hostname
-    docker run -e PEERS=10 -e CONNECTTO=5 --hostname="$hostname" --network="$custom_network_name" libp2pnode &
+    docker run -e PEERS=10 -e CONNECTTO=5 --hostname="$hostname" --network="$custom_network_name" dst-test-node &
+
+    sleep 2
 done
