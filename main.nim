2711eebeb77651424fabff54b0ef523e340a41eb
akshaya <AkshayaMani@users.noreply.github.com>
2025-03-28 11:07:51 -0400

    Updated `createSwitch` and added callback to GossipSub init

--- main.nim
+++ main.nim
@@ -18,21 +18,45 @@ import
 from times import getTime, toUnix, fromUnix, `-`, initTime, `$`, inMilliseconds
 from nativesockets import getHostname
 
-const chunks = 1
+proc createSwitch(id, port: int): Switch =
+  {.gcsafe.}:
+    discard initializeMixNodes(1, port)
 
-proc createSwitch(libp2pPrivKey: SkPrivateKey, multiAddr: MultiAddress): Switch =
-  let
-    inTimeout: Duration = 5.minutes
-    outTimeout: Duration = 5.minutes
-  result = SwitchBuilder
-    .new()
-    .withPrivateKey(PrivateKey(scheme: Secp256k1, skkey: libp2pPrivKey))
-    .withAddress(multiAddr)
-    .withRng(crypto.newRng())
-    .withMplex(inTimeout, outTimeout)
-    .withTcpTransport()
-    .withNoise()
-    .build()
+    let nodePubInfo = getMixPubInfoByIndex(0).valueOr:
+      error "Get mix pub info by index error", err = error
+      return
+
+    let writePubRes = writePubInfoToFile(nodePubInfo, id)
+    if writePubRes.isErr:
+      error "Failed to write pub info to file", nodeId = id
+      return
+
+    let writeNodeRes = writeMixNodeInfoToFile(mixNodes[0], id)
+    if writeNodeRes.isErr:
+      error "Failed to write mix info to file", nodeId = id
+      return
+
+    let (multiAddrStr, _, _, _, libp2pPrivKey) = getMixNodeInfo(mixNodes[0])
+
+    let multiAddr = MultiAddress.init(multiAddrStr.split("/p2p/")[0]).valueOr:
+      error "Failed to initialize MultiAddress", err = error
+      return
+
+    let switch = SwitchBuilder
+      .new()
+      .withPrivateKey(PrivateKey(scheme: Secp256k1, skkey: libp2pPrivKey))
+      .withAddress(multiAddr)
+      .withRng(crypto.newRng())
+      .withMplex()
+      .withTcpTransport()
+      .withNoise()
+      .build()
+
+    if switch.isNil:
+      warn "Failed to set up node", nodeId = id
+      return
+
+    return switch
 
 proc msgIdProvider(m: Message): Result[MessageId, ValidationResult] =
   return ok(($m.data.hash).toBytes())
@@ -61,46 +85,30 @@ proc main() {.async.} =
     myId = parseInt(getEnv("PEERNUMBER"))
     msg_rate = parseInt(getEnv("MSGRATE"))
     msg_size = parseInt(getEnv("MSGSIZE"))
-
-    #publisherCount = client.param(int, "publisher_count")
     publisherCount = parseInt(getEnv("PEERS"))
     isPublisher = myId <= publisherCount
-    #isAttacker = (not isPublisher) and myId - publisherCount <= client.param(int, "attacker_count")
-    isAttacker = false
     rng = libp2p.newRng()
-    #randCountry = rng.rand(distribCumSummed[^1])
-    #country = distribCumSummed.find(distribCumSummed.filterIt(it >= randCountry)[0])
   echo "Hostname: ", hostname
   let
     myport = 5000 + parseInt(getEnv("PEERNUMBER"))
-    myaddress = "0.0.0.0:" & $myport
-    address = initTAddress(myaddress)
-    switch = SwitchBuilder
-      .new()
-      .withAddress(MultiAddress.init(address).tryGet())
-      .withRng(rng)
-      #.withYamux()
-      .withMplex()
-      .withMaxConnections(250)
-      .withTcpTransport(flags = {ServerFlags.TcpNoDelay})
-      #.withPlainText()
-      .withNoise()
-      .build()
+    switch = createSwitch(myId, myport)
     gossipSub = GossipSub.init(
       switch = switch,
-      #      triggerSelf = true,
+      triggerSelf = true,
       msgIdProvider = msgIdProvider,
       verifySignature = false,
       anonymize = true,
+      customConnCallbacks = some(
+        CustomConnectionCallbacks(
+          customConnCreationCB: mixConn, peerSelectionCB: mixPeerSelect
+        )
+      ),
     )
-    pingProtocol = Ping.new(rng = rng)
   # Metrics
   echo "Starting metrics HTTP server"
   let metricsServer = startMetricsServer(parseIpAddress("0.0.0.0"), Port(8008))
 
   gossipSub.parameters.floodPublish = true
-  #gossipSub.parameters.lazyPushThreshold = 1_000_000_000
-  #gossipSub.parameters.lazyPushThreshold = 0
   gossipSub.parameters.opportunisticGraftThreshold = -10000
   gossipSub.parameters.heartbeatInterval = 1.seconds
   gossipSub.parameters.pruneBackoff = 60.seconds
@@ -118,17 +126,12 @@ proc main() {.async.} =
     firstMessageDeliveriesDecay: 0.9,
   )
 
-  var messagesChunks: CountTable[uint64]
   proc messageHandler(topic: string, data: seq[byte]) {.async.} =
     let sentUint = uint64.fromBytesLE(data)
     # warm-up
     if sentUint < 1000000:
       return
-    #if isAttacker: return
 
-    messagesChunks.inc(sentUint)
-    if messagesChunks[sentUint] < chunks:
-      return
     let
       sentMoment = nanoseconds(int64(uint64.fromBytesLE(data)))
       sentNanosecs = nanoseconds(sentMoment - seconds(sentMoment.seconds))
@@ -136,40 +139,20 @@ proc main() {.async.} =
       diff = getTime() - sentDate
     echo sentUint, " milliseconds: ", diff.inMilliseconds()
 
-  var
-    startOfTest: Moment
-    attackAfter = 10000.hours
   proc messageValidator(
       topic: string, msg: Message
   ): Future[ValidationResult] {.async.} =
-    if isAttacker and Moment.now - startOfTest >= attackAfter:
-      return ValidationResult.Ignore
-
     return ValidationResult.Accept
 
   gossipSub.subscribe("test", messageHandler)
   gossipSub.addValidator(["test"], messageValidator)
   switch.mount(gossipSub)
-  switch.mount(pingProtocol)
   await switch.start()
-  #defer: await switch.stop()
 
   echo "Listening on ", switch.peerInfo.addrs
   echo myId, ", ", isPublisher, ", ", switch.peerInfo.peerId
   echo "Waiting 60 seconds for node building..."
   await sleepAsync(60.seconds)
-
-  proc pinger(peerId: PeerId) {.async.} =
-    try:
-      await sleepAsync(20.seconds)
-      while true:
-        let stream = await switch.dial(peerId, PingCodec)
-        let delay = await pingProtocol.ping(stream)
-        await stream.close()
-        #echo delay
-        await sleepAsync(delay)
-    except:
-      echo "Failed to ping"
 
   let connectTo = parseInt(getEnv("CONNECTTO"))
   var connected = 0
@@ -197,7 +180,6 @@ proc main() {.async.} =
         echo "Trying to connect to ", addrs[index]
         let peerId =
           await switch.connect(addrs[index], allowUnknownPeerId = true).wait(5.seconds)
-        #asyncSpawn pinger(peerId)
         connected.inc()
         index.inc()
         echo "Connected!"
@@ -206,11 +188,6 @@ proc main() {.async.} =
         echo "Failed to dial", exc.msg
         echo "Waiting 15 seconds..."
         await sleepAsync(15.seconds)
-
-  #let
-  #  maxMessageDelay = client.param(int, "max_message_delay")
-  #  warmupMessages = client.param(int, "warmup_messages")
-  #startOfTest = Moment.now() + milliseconds(warmupMessages * maxMessageDelay div 2)
 
   echo "Mesh size: ", gossipSub.mesh.getOrDefault("test").len
 
@@ -225,8 +202,5 @@ proc main() {.async.} =
         nowInt = seconds(now.toUnix()) + nanoseconds(times.nanosecond(now))
       var nowBytes = @(toBytesLE(uint64(nowInt.nanoseconds))) & newSeq[byte](msg_size)
       doAssert((await gossipSub.publish("test", nowBytes)) > 0)
-
-  #echo "BW: ", libp2p_protocols_bytes.value(labelValues=["/meshsub/1.1.0", "in"]) + libp2p_protocols_bytes.value(labelValues=["/meshsub/1.1.0", "out"])
-  #echo "DUPS: ", libp2p_gossipsub_duplicate.value(), " / ", libp2p_gossipsub_received.value()
 
 waitFor(main())
